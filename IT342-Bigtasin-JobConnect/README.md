c9e005dff0d83f993983a3d37eea69fe003534ae
LAB13_USER ON WS08 <L13Y17W08@LabHCI.net>
2026-03-07 15:27:43 +0800

    IT342 Phase 1 - User Registration and Login Completed

---
+++ IT342-Bigtasin-JobConnect/README.md
@@ -0,0 +1,1 @@
+# IT342-Bigtasin-JobConnect
---
+++ IT342-Bigtasin-JobConnect/backend/src/main/java/edu/cit/bigtasin/jobconnect/JobconnectApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.bigtasin.jobconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobconnectApplication.class, args);
+	}
+
+}
