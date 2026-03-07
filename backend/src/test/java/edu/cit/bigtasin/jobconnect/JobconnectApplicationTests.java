b5a6f1c39bebfd253b4f6855eaf4efdd03c226c3
LAB13_USER ON WS08 <L13Y17W08@LabHCI.net>
2026-03-07 16:08:37 +0800

    Cleanup: Removed IDE config and internal library files for clean Supabase restart

---
+++ backend/src/test/java/edu/cit/bigtasin/jobconnect/JobconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.bigtasin.jobconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
